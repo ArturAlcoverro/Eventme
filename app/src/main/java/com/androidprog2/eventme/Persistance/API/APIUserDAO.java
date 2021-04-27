fa04f1e7f73942e21b95614c229a6c13b8d663b1
David Marquet <david.marquet@students.salle.url.edu>
2021-04-27 19:18:05 +0200

    look @TODO

--- app/src/main/java/com/androidprog2/eventme/Persistance/API/APIUserDAO.java
+++ app/src/main/java/com/androidprog2/eventme/Persistance/API/APIUserDAO.java
@@ -6,33 +6,36 @@ import com.androidprog2.eventme.business.User;
 import java.util.ArrayList;
 
 public class APIUserDAO implements UserDAO {
+
     @Override
     public void createUser(User user) {
-
+        //APIConnector.getInstance().postApi();
     }
 
     @Override
     public void loginUser(User user) {
-
+        APIConnector.getInstance().postApi();
     }
 
     @Override
     public ArrayList<User> getAllUsers() {
+        APIConnector.getInstance().getApi();
         return null;
     }
 
     @Override
     public User searchById(int id) {
+        APIConnector.getInstance().getApi();
         return null;
     }
 
     @Override
     public void changeUser(User user) {
-
+        APIConnector.getInstance().putApi();
     }
 
     @Override
     public void deleteUser() {
-
+        APIConnector.getInstance().deleteApi();
     }
 }
