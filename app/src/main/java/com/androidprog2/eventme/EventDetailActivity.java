8a3ec60e7123256fb0555eaf4b115ce3cd3d98ad
Artur Alcoverro Pastó <artur.alcoverro@students.salle.url.edu>
2021-05-04 18:00:31 +0200

    Events detail UI

---
+++ app/src/main/java/com/androidprog2/eventme/EventDetailActivity.java
@@ -0,0 +1,14 @@
+package com.androidprog2.eventme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail);
+    }
+}
