47470540f315ebcac629502f60ccef840915b5b6
David Marquet <david.marquet@students.salle.url.edu>
2021-05-13 18:23:33 +0200

    fix merge

--- app/src/main/java/com/androidprog2/eventme/presentation/activities/StartActivity.java
+++ app/src/main/java/com/androidprog2/eventme/presentation/activities/StartActivity.java
@@ -21,8 +21,9 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
-        createUser(new User(0, "https://ichef.bbci.co.uk/news/800/cpsprodpb/F1F2/production/_118283916_b19c5a1f-162b-410b-8169-f58f0d153752.jpg", "david2", "Marquet2", "test2@hotmail.com", "david123"));
+        createUser(new User(0,
+                "https://ichef.bbci.co.uk/news/800/cpsprodpb/F1F2/production/_118283916_b19c5a1f-162b-410b-8169-f58f0d153752.jpg",
+                "david2", "Marquet2", "test2@hotmail.com", "david123"));
     }
 
     private void createUser(User user) {
